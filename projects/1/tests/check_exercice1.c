695e50a2f8901344134e84e82ac888bbb1c3d5a5
Julien Boriasse <julien@boriasse.com>
2022-08-18 12:31:12 -0400

    Update check_exercice1.c

--- projects/1/tests/check_exercice1.c
+++ projects/1/tests/check_exercice1.c
@@ -8,7 +8,31 @@ START_TEST (exercice1)
 }
 END_TEST
 
+Suite * exercice1_suite(void) {
+  Suite *s;
+  TCase *tc_core;
+
+  s = suite_create("Exercice1");
+
+  tc_core = tcase_create("Core");
+
+  tcase_add_test(tc_core, test_exercice1_create);
+  suite_add_tcase(s, tc_core);
+
+  return s;
+}
+
 int main(void)
 {
-    return 0;
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = exercice1_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
